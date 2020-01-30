48dd4ff87eecafde8d34aadbe394231c4243e727
Menno <menno.hoekstra@pxlwidgets.com>
2020-01-30 21:38:06 +0100

    [Update] Remove unused code

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -53,9 +53,5 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::
-             middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
     }
 }

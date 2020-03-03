42d638e033246472fc88b1c91ad4f6f996419215
Menno <menno.hoekstra@pxlwidgets.com>
2020-03-03 22:20:44 +0100

    [Update] Update render and report with Throwable instance

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,22 +2,19 @@
 
 namespace App\Exceptions;
 
-use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
-
-
     /**
      * A list of the exception types that are not reported.
      *
      * @var array
      */
-    protected $dontReport = [];
-
+    protected $dontReport = [
+        //
+    ];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
@@ -30,13 +27,28 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * Report or log an exception.
+     *
+     * @param  \Throwable  $exception
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function report(Throwable $exception)
+    {
+        parent::report($exception);
+    }
+
+    /**
      * Render an exception into an HTTP response.
      *
-     * @param Request   $request
-     * @param Exception $exception
-     * @return Response
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
